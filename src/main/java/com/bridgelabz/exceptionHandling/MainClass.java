7a8ca52e938089cf5080c5b8efba4b22c34eb458
HarishKamaras <harishkamarasu@gmail.com>
2022-06-02 17:34:53 +0530

    PasswordRule3

--- src/main/java/com/bridgelabz/exceptionHandling/MainClass.java
+++ src/main/java/com/bridgelabz/exceptionHandling/MainClass.java
@@ -96,6 +96,21 @@ public class MainClass {
             System.out.println("Invaild, please enter one upper case and minimum 8 character");
         }
     }
+    public static void passwordRule3() {
+        Scanner scanner = new Scanner(System.in);
+        String PasswordPattern = "^(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9@#$%^&+]{8,}$";
+        System.out.println("Enter the Password");
+        String passWord = scanner.next();
+        Pattern pattern = Pattern.compile(PasswordPattern);
+        Matcher matcher = pattern.matcher(passWord);
+        Boolean result = matcher.matches();
+
+        if (result == true) {
+            System.out.println("Password is valid");
+        } else {
+            System.out.println("Invaild, please enter one number and minimum 8 character");
+        }
+    }
         public static void main(String[] args) {
         System.out.println("Welcome to Exception Handling");
         firstName();//UC1
@@ -104,5 +119,6 @@ public class MainClass {
         mobileNumber();//UC4
         passwordRule1();//UC5
         passwordRule2();//UC6
+        passwordRule3();//UC7
     }
 }
