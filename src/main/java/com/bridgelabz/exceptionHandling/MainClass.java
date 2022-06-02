697f0b996cb43a4a266e4e499cb1a11b488645db
HarishKamaras <harishkamarasu@gmail.com>
2022-06-02 17:11:23 +0530

    LastName

--- src/main/java/com/bridgelabz/exceptionHandling/MainClass.java
+++ src/main/java/com/bridgelabz/exceptionHandling/MainClass.java
@@ -22,11 +22,24 @@ public class MainClass {
         else
             System.out.println("First Name is Invalid");
     }
+    public static void lastName() {
 
+        String lastNamePattern = "[A-Z]{1}[a-z]{2,}";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Last Name:");
+        String lastName = scanner.next();
+        Pattern pattern = Pattern.compile(lastNamePattern);
+        Matcher matcher = pattern.matcher(lastName);
+        boolean result = matcher.matches();
 
-
+        if(result == true)
+            System.out.println("Last Name is Valid");
+        else
+            System.out.println("Last Name is Invalid");
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Exception Handling");
         firstName();//UC1
+        lastName();//UC2
     }
 }
