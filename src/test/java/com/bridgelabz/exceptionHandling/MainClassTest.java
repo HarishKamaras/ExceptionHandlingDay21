b76af03dc03b32bb4aba5bcfc5a7c2e2ad0f0ef1
HarishKamaras <harishkamarasu@gmail.com>
2022-06-02 17:01:41 +0530

    Exception

---
+++ src/test/java/com/bridgelabz/exceptionHandling/MainClassTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.exceptionHandling;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainClassTest {
+
+    /*@Test
+    public void userFirstNameShouldPassTheRegexValidation() {
+        MainClass obj = new MainClass();
+        boolean result = obj.firstName("Arish");
+        assertEquals("fName",true,result);
+    }*/
+}
